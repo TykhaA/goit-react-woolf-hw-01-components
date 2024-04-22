ce285bb06594a15a5cddbf28e727502f06604fd4
Anastasiia Tykha <anastasiia.tykha@academysmart.com>
2024-04-22 14:35:08 +0300

    remode dublicate

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,7 +9,7 @@ const Statistics = ({stats, title}) => {
       </div>
       <ul className={style.stats_list}>
       {stats.map((elem) =>{
-        return (<li className={style.item} key={elem.id} style={{backgroundColor: generateRandomColor(), width:(100 / stats.length) + '%'}} key={elem.id}>
+        return (<li className={style.item} style={{backgroundColor: generateRandomColor(), width:(100 / stats.length) + '%'}} key={elem.id}>
           <span  className={style.label}>{elem.label}</span>
           <span className={style.percentage}>{elem.percentage}%</span>
         </li>)
